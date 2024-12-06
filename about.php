92d12ffed6d450f50c397bd514dacd7e4633b705
Georgio Hayek <georgiohayek2002@gmail.com>
2024-12-06 15:18:36 +0200

    elfishgt add file

--- about.php
+++ about.php
@@ -133,7 +133,8 @@ include_once "./layout/header.php";
         <h1 class="viral-main-title">Viral Videos</h1>
         <div class="title-pink-line"></div>
         <div class="viral-videos-container">
-            <script src="https://static.elfsight.com/platform/platform.js" async></script>
+            <!-- <script src="https://static.elfsight.com/platform/platform.js" async></script> -->
+             <script src="assets/js/elfsight.js"></script>
             <div class="elfsight-app-9f538779-ee19-4dd9-9e6d-2db1001ffbd3" data-elfsight-app-lazy></div>
         </div>
     </div>
