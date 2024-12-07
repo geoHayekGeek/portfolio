7f9f008ac6e3e3d898a4d4def7242ad394dbce76
Georgio Hayek <georgiohayek2002@gmail.com>
2024-12-07 18:26:27 +0200

    creating the work page

--- work.php
+++ work.php
@@ -1,9 +1,161 @@
-<?php 
+<?php
 include_once "./layout/header.php";
 ?>
 
-<h1>Work</h1>
+<section class="container-fluid about-section" id="work_section">
+    <div class="site-map">
+        <div class="sitemap-page">
+            <img src="assets\img\work\sitemap_single.png" loading="lazy" alt="page-icon" class="sitemap-image">
+            <h4 class="sitemap-title">My Work</h4>
+        </div>
+        <div class="sitemap-info">
+            <div class="sitemap-text">inspiring selection</div>
+        </div>
+    </div>
 
-<?php 
+    <div class="work-wrapper">
+        <div class="work-list-wrapper w-dyn-list">
+            <div role="list" class="work-list w-dyn-items w-row">
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Essence</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                branding </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work2.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Elixir</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                painting </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work1.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Zenith</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                photography </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work3.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Harmony</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                branding </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work5.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Quassar</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                3D </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work6.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Symmetry</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                workspace </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work8-1.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Momentum</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                photography </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work8.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Fussion</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                wallpaper </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work5.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+
+                <div role="listitem" class="work-item w-dyn-item w-col w-col-4">
+                    <a href="" class="work-item-inner w-inline-block">
+                        <h4 class="work-title">Serenity</h4>
+                        <div class="work-category">
+                            <span class="cat-link">
+                                branding </span>
+                        </div>
+                        <div class="image-holder">
+                            <img decoding="async"
+                                src="https://themeguri.com/gridos/wp-content/uploads/sites/2/2024/01/work4.jpg"
+                                loading="lazy" alt="work-image" class="work-image">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
 include_once "./layout/footer.php";
 ?>
