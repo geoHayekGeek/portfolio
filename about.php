c2de4049830d527ab5d85975dad49fc3ae87fab8
Georgio Hayek <georgiohayek2002@gmail.com>
2024-12-07 18:32:27 +0200

    addik tiktok section in work

--- about.php
+++ about.php
@@ -129,15 +129,10 @@ include_once "./layout/header.php";
 
     <div class="space-line"></div>
 
-    <div class="viral-block">
-        <h1 class="viral-main-title">Viral Videos</h1>
-        <div class="title-pink-line"></div>
-        <div class="viral-videos-container">
-            <!-- <script src="https://static.elfsight.com/platform/platform.js" async></script> -->
-             <script src="assets/js/elfsight.js"></script>
-            <div class="elfsight-app-9f538779-ee19-4dd9-9e6d-2db1001ffbd3" data-elfsight-app-lazy></div>
-        </div>
-    </div>
+    <?php
+    include_once "./partials/tiktok_section.php";
+    ?>
+
 </section>
 
 <?php
---
+++ partials/tiktok_section.php
@@ -0,0 +1,9 @@
+<div class="viral-block">
+        <h1 class="viral-main-title">Viral Videos</h1>
+        <div class="title-pink-line"></div>
+        <div class="viral-videos-container">
+            <!-- <script src="https://static.elfsight.com/platform/platform.js" async></script> -->
+             <script src="assets/js/elfsight.js"></script>
+            <div class="elfsight-app-9f538779-ee19-4dd9-9e6d-2db1001ffbd3" data-elfsight-app-lazy></div>
+        </div>
+    </div>
